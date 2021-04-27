66010f4edd05de1baa1d09ac9d21cade00af454c
Sincerity7788 <dei@live.cn>
2021-04-27 11:02:28 +0800

    增加了getDate获取时间格式的方法

--- index.js
+++ index.js
@@ -89,4 +89,74 @@ lc_tools.toJson = json => {
   return res;
 };
 
+/**
+ * @param { string } format
+ * @param { Date | number } time
+ * @return { string } res
+ * */
+lc_tools.getDate = (format = "Y-M-D", time = new Date()) => {
+  // 验证当前从传入的时间格式是否正确
+  try {
+    time = new Date(time);
+    if (isNaN(time.getDate())) {
+      throw new Error("时间格式错误！");
+    }
+  } catch (e) {
+    console.error(e);
+    return;
+  }
+
+  // 获取时间的函数
+  let getTime = t => {
+    let strTime = "";
+    switch (t) {
+      case "Y":
+        // 获取年
+        strTime = time.getFullYear();
+        break;
+      case "M":
+        // 获取月
+        strTime = time.getMonth() + 1 + "";
+        strTime = strTime.length > 1 ? strTime : "0" + strTime;
+        break;
+      case "D":
+        // 获取日
+        strTime = time.getDate() + "";
+        strTime = strTime.length > 1 ? strTime : "0" + strTime;
+        break;
+      case "h":
+        // 获取小时
+        strTime = time.getHours() + "";
+        strTime = strTime.length > 1 ? strTime : "0" + strTime;
+        break;
+      case "m":
+        // 获取分钟
+        strTime = time.getMinutes() + "";
+        strTime = strTime.length > 1 ? strTime : "0" + strTime;
+        break;
+      case "s":
+        // 获取秒
+        strTime = time.getSeconds() + "";
+        strTime = strTime.length > 1 ? strTime : "0" + strTime;
+        break;
+      default:
+        strTime = t;
+    }
+    return strTime;
+  };
+
+  let res = "";
+  // 循环获取
+  for (let i = 0, len = format.length; i < len; i++) {
+    let t = getTime(format[i]);
+    if (t !== "error") {
+      res += t;
+    } else {
+      console.error(new Error("请传入正确的时间格式！"));
+      return "error";
+    }
+  }
+  return res;
+};
+
 export default lc_tools;
